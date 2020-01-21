a9c16f703d4c2103abf80cadcd72cdcc04d2f5ad
Somaia Khalil <sumaya_khair@live.com>
2020-01-21 17:44:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,42 +1,21 @@
-require 'pp'
 def nyc_pigeon_organizer(data)
-  newhash = {}
-  data.each do |key,value|
-    value.each do |newkey,newvalue|
-     newvalue.each do |name|
-       if !newhash[name]
-         newhash[name]={}
-         
-       end
-     end
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+        
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+
+        new_hash[name][key] << new_value.to_s
+
+      end
     end
   end
-  puts newhash
+  new_hash
 end
-
-
-
-
-
-
-
-
-nyc_pigeon_organizer(pigeon_data = {
-  :color => {
-    :purple => ["Theo", "Peter Jr.", "Lucky"],
-    :grey => ["Theo", "Peter Jr.", "Ms. K"],
-    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-    :brown => ["Queenie", "Alex"]
-  },
-  :gender => {
-    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-    :female => ["Queenie", "Ms. K"]
-  },
-  :lives => {
-    "Subway" => ["Theo", "Queenie"],
-    "Central Park" => ["Alex", "Ms. K", "Lucky"],
-    "Library" => ["Peter Jr."],
-    "City Hall" => ["Andrew"]
-  }
-})
-
