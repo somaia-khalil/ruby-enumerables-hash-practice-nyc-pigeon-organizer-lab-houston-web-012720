d2fe6facc976c3a8568e88a8deb5c31f691bfa3c
Somaia Khalil <sumaya_khair@live.com>
2020-01-21 16:00:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,7 @@ def nyc_pigeon_organizer(data)
       |key,name|
       name.each do
         if !newhash[name]
-          newhash[name]= name
+          newhash[name]= {}
         end
       end
     end
